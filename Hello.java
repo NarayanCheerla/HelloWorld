7f15d71f21be27ebd7d7958d9579dd9762c87a5e
NACHEERL <NACHEERL@NACHEERL-T490.in.oracle.com>
2021-11-20 14:58:42 +0530

    third commit

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 	public static void main(String[] args){
-		for(int i=0;i<5;i++){
-			System.out.println("Iteration :: "+i);
+		for(int i=0;i<10;i++){
+			System.out.println("Iteration No :: "+i);
 		}
 	}
 }
